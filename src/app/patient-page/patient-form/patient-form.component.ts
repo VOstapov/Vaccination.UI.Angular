e24b39f66066db74abacbad1c37d017e49a63486
vladislav.ostapov <vladislav.ostapov@r-express.ru>
2019-09-09 14:15:21 +0300

    валидация снилс

--- src/app/patient-page/patient-form/patient-form.component.ts
+++ src/app/patient-page/patient-form/patient-form.component.ts
@@ -26,7 +26,7 @@ export class PatientFormComponent implements OnInit {
       'name': new FormControl(null, [Validators.required]),
       'patronomic': new FormControl(null, [Validators.required]),
       'birthday': new FormControl(null, [Validators.required, this.checkForCorrectDate.bind(this)]),
-      'snils': new FormControl(null, [Validators.required]),
+      'snils': new FormControl(null, [Validators.required, this.snilsValidator.bind(this)]),
       'gender': new FormControl("Мужской", [Validators.required])
     });
 
@@ -66,5 +66,62 @@ export class PatientFormComponent implements OnInit {
     }
 
     return null;
+  }
+
+
+  snilsValidator(control: FormControl) {
+/*
+    1. Проверка контрольного числа Страхового номера проводится только для номеров больше номера 001-001-998
+    2. Контрольное число СНИЛС рассчитывается следующим образом:
+    2.1. Каждая цифра СНИЛС умножается на номер своей позиции (позиции отсчитываются с конца)
+    2.2. Полученные произведения суммируются
+    2.3. Если сумма меньше 100, то контрольное число равно самой сумме
+    2.4. Если сумма равна 100 или 101, то контрольное число равно 00
+    2.5. Если сумма больше 101, то сумма делится по остатку на 101 и контрольное число определяется остатком от деления аналогично пунктам 2.3 и 2.4
+*/
+    if (!control.value) {
+      return {
+        "empty": true
+      };
+    }
+
+    const snils: string = control.value.replace(/[^0-9]/g,'');
+    if (snils.length != 11) {
+      return {
+        'incorrectLenght' : true
+      };
+    }
+
+    const number = snils.substr(0, 9);
+    const checksum = snils.substr(9, 2);
+
+    if (+number <= 1001998) {
+      return null;
+    }
+
+    let sum = 0;
+    let i = 9;
+    for (let s of number) {
+      sum += i-- * +s;
+    }
+
+    if (this.checkControlSum(sum, +checksum)) {
+      return null;
+    }
+
+    if (sum > 101) {
+      let devided = sum % 101;
+      if (this.checkControlSum(devided, +checksum)) {
+        return null;
+      }
+    }
+
+    return {
+      'incorrectChecksum': true
+    };
+  }
+
+  checkControlSum(sum: number, checksum: number) : boolean {
+    return ((sum < 100 && sum === checksum) || ((sum === 100 || sum === 101) && checksum === 0));
   }
 }
